f609ff32b4a868217cbe59a11fdb41e08652fe53
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2023-03-02 11:26:55 +0300

    added definition of surfaces

--- ARObjectsApp/ViewController.swift
+++ ARObjectsApp/ViewController.swift
@@ -9,14 +9,18 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class ViewController: UIViewController {
 
     @IBOutlet var sceneView: ARSCNView!
+    var planes = [Plane]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         sceneView.delegate = self
+        
         sceneView.showsStatistics = true
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        sceneView.autoenablesDefaultLighting = true
         
         let scene = SCNScene()
         
@@ -27,11 +31,34 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         super.viewWillAppear(animated)
         
         let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
+        
         sceneView.session.run(configuration)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         sceneView.session.pause()
+    }
+}
+
+// MARK: - ARSCNViewDelegate
+extension ViewController: ARSCNViewDelegate {
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard anchor is ARPlaneAnchor else { return }
+        
+        let plane = Plane(anchor: anchor as! ARPlaneAnchor)
+        
+        self.planes.append(plane)
+        node.addChildNode(plane)
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        let plane = self.planes.filter { plane in
+            return plane.anchor.identifier == anchor.identifier
+        }.first
+        
+        guard plane != nil else { return }
+        plane?.update(anchor: anchor as! ARPlaneAnchor)
     }
 }
